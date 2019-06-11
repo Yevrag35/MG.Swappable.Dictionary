47b0b3ac7324de6c24202ae3677ec204e9a639e9
Mike Garvey <mike.garvey@yevrag35.com>
2019-06-11 19:10:07 -0400

    Add project files.

---
+++ MG.Swappable.Dictionary/DictionarySwap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MG.Swappable
+{
+    internal class DictionarySwap : ISwappable
+    {
+        string ISwappable.NewKey { get; set; }
+        string ISwappable.OldKey { get; set; }
+        object ISwappable.Value { get; set; }
+
+        private DictionarySwap(string newKey, KeyValuePair<string, object> oldKvp)
+        {
+            ((ISwappable)this).NewKey = newKey;
+            ((ISwappable)this).OldKey = oldKvp.Key;
+            ((ISwappable)this).Value = oldKvp.Value;
+        }
+
+        KeyValuePair<string, object> ISwappable.AsNewPair()
+        {
+            return new KeyValuePair<string, object>(((ISwappable)this).NewKey, ((ISwappable)this).Value);
+        }
+
+        internal static ISwappable New(string newKey, KeyValuePair<string, object> oldKvp) => new DictionarySwap(newKey, oldKvp);
+    }
+}
---
+++ MG.Swappable.Dictionary/Interfaces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MG.Swappable
+{
+    public interface ISwappable
+    {
+        string NewKey { get; set; }
+        string OldKey { get; set; }
+        object Value { get; set; }
+
+        KeyValuePair<string, object> AsNewPair();
+    }
+
+    public interface ISwappableDictionary
+    {
+        bool ContainsKey(string key, StringComparison comparison);
+        bool TryGetKey(string key, StringComparison comparison, out string outKey);
+        bool TryGetKvp(string key, StringComparison comparison, out KeyValuePair<string, object> outKvp);
+
+        ISwappable NewSwappable(string newKey, string oldKey, StringComparison comparison);
+        void Swap(string newKey, StringComparison comparison);
+        void Swap(ISwappable swapWith);
+    }
+}
